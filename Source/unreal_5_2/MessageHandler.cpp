d1f1369b054c6944e04f96bb7f3b43994d49ffa7
KaiMJ <mjkim2964@gmail.com>
2024-07-09 23:57:02 -0700

    add object validator

--- Source/unreal_5_2/MessageHandler.cpp
+++ Source/unreal_5_2/MessageHandler.cpp
@@ -28,9 +28,13 @@ FString FMessageHandler::ParseAndDispatch(TSharedPtr<FJsonObject>& JsonObject)
 {
     FString Action = JsonObject->GetStringField(TEXT("action"));
     FString message = "Nothing happened";
+
     if (Action == TEXT("add_object"))
     {
-        // TODO: check correct format
+        if (!ValidateJsonData(JsonObject, message))
+        {
+            return message;
+        }
         message = ObjectManager->SpawnObject(JsonObject);
     }
     // ToDo: More info
@@ -40,4 +44,53 @@ FString FMessageHandler::ParseAndDispatch(TSharedPtr<FJsonObject>& JsonObject)
 void FMessageHandler::AddObjectManager(UObjectManager* Manager)
 {
     ObjectManager = Manager;
+}
+
+bool FMessageHandler::ValidateJsonData(TSharedPtr<FJsonObject>& JsonData, FString& ErrorMessage)
+{
+    if (!JsonData.IsValid())
+    {
+        ErrorMessage = TEXT("Invalid JSON data!");
+        return false;
+    }
+
+    TSharedPtr<FJsonObject> LocationObject = JsonData->GetObjectField(TEXT("location"));
+    if (!LocationObject.IsValid() ||
+        !LocationObject->HasTypedField<EJson::Number>(TEXT("x")) ||
+        !LocationObject->HasTypedField<EJson::Number>(TEXT("y")) ||
+        !LocationObject->HasTypedField<EJson::Number>(TEXT("z")))
+    {
+        ErrorMessage = TEXT("Invalid location data!");
+        return false;
+    }
+
+    TSharedPtr<FJsonObject> RotationObject = JsonData->GetObjectField(TEXT("rotation"));
+    if (!RotationObject.IsValid() ||
+        !RotationObject->HasTypedField<EJson::Number>(TEXT("pitch")) ||
+        !RotationObject->HasTypedField<EJson::Number>(TEXT("yaw")) ||
+        !RotationObject->HasTypedField<EJson::Number>(TEXT("roll")))
+    {
+        ErrorMessage = TEXT("Invalid rotation data!");
+        return false;
+    }
+
+    if (!JsonData->HasTypedField<EJson::String>(TEXT("material_path")))
+    {
+        ErrorMessage = TEXT("Invalid or missing material_path!");
+        return false;
+    }
+
+    if (!JsonData->HasTypedField<EJson::String>(TEXT("mesh_path")))
+    {
+        ErrorMessage = TEXT("Invalid or missing mesh_path!");
+        return false;
+    }
+
+    if (!JsonData->HasTypedField<EJson::Boolean>(TEXT("physics")))
+    {
+        ErrorMessage = TEXT("Invalid or missing physcis!");
+        return false;
+    }
+
+    return true;
 }
--- Source/unreal_5_2/ObjectManager.cpp
+++ Source/unreal_5_2/ObjectManager.cpp
@@ -17,11 +17,6 @@ FString UObjectManager::SpawnObject(TSharedPtr<FJsonObject>& JsonData)
 {
     //TODO: Keep track of unique object_id in ObjectDatabase
 
-    // Implementation of SpawnObject method
-    UE_LOG(LogTemp, Log, TEXT("Spawn Object called"));
-    //FString ObjectClassString = JsonObject->GetStringField(TEXT("object_class"));
-    //UClass* ObjectClass = FindObject<UClass>(ANY_PACKAGE, *ObjectClassString);
-
     FVector Location;
     Location.X = JsonData->GetObjectField(TEXT("location"))->GetNumberField(TEXT("x"));
     Location.Y = JsonData->GetObjectField(TEXT("location"))->GetNumberField(TEXT("y"));
@@ -31,19 +26,10 @@ FString UObjectManager::SpawnObject(TSharedPtr<FJsonObject>& JsonData)
     Rotation.Pitch = JsonData->GetObjectField(TEXT("rotation"))->GetNumberField(TEXT("pitch"));
     Rotation.Yaw = JsonData->GetObjectField(TEXT("rotation"))->GetNumberField(TEXT("yaw"));
     Rotation.Roll = JsonData->GetObjectField(TEXT("rotation"))->GetNumberField(TEXT("roll"));
-    // Convert FVector and FRotator to FString
-    FString LocationString = Location.ToString();
-    FString RotationString = Rotation.ToString();
 
-    // Log the values using UE_LOG
-    UE_LOG(LogTemp, Log, TEXT("Location: %s"), *LocationString);
-    UE_LOG(LogTemp, Log, TEXT("Rotation: %s"), *RotationString);
     FActorSpawnParameters SpawnParams;
 
     AActor* SpawnedActor = ThisWorld->SpawnActor<AActor>(AActor::StaticClass(), Location, Rotation, SpawnParams);
-    FVector ActualLocation = SpawnedActor->GetActorLocation();
-
-    UE_LOG(LogTemp, Log, TEXT("Actual Location: %s"), *ActualLocation.ToString());
 
 
     FString MaterialPath = JsonData->GetStringField(TEXT("material_path"));
@@ -61,12 +47,14 @@ FString UObjectManager::SpawnObject(TSharedPtr<FJsonObject>& JsonData)
             UMaterial* Material = Cast<UMaterial>(StaticLoadObject(UMaterial::StaticClass(), nullptr, *MaterialPath));
             MyMesh->SetMaterial(0, Material);
         }
-        //Set additional properties for the mesh component
-        MyMesh->SetSimulatePhysics(true);
-        MyMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
-        MyMesh->SetCollisionProfileName(TEXT("BlockAll"));
 
-        //Attach the mesh component to the actor's root component
+        if (JsonData->GetBoolField(TEXT("physics"))) {
+            MyMesh->SetSimulatePhysics(true);
+            MyMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+            MyMesh->SetCollisionProfileName(TEXT("BlockAll"));
+        }
+
+
         MyMesh->SetupAttachment(SpawnedActor->GetRootComponent());
         MyMesh->RegisterComponent();
     }
